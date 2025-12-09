9bccc90a1ae77e82b4b75516d031094bfb0d365c
Nicky <dovern42@gmail.com>
2025-12-08 22:18:58 -0500

    add articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,101 @@
 
 Article.destroy_all
 
+content = <<-TEXT
+
+<p>You know our motto: WE NEED A GOOD LAWYER, FAST.</p>
+
+<p>Are you a lawyer? Can you convincingly play one on TV? Are you someone's cousin Vinny?</p>
+
+<p>If you're an underage supe, the brass upstairs makes you sign a contract if you want to do some crimefighting. They spun us a yarn about how we would be protected from liabilities, we'd have some cash thrown our way for each villain we throw their way, etc. etc.</p>
+
+<p>Our perspective on lawyers is to remove yourself from them as quickly as possible, and it seemed like the best way was to sign the contract. But now we're wondering: Are we doing anything illegal by sharing this zine?</p>
+
+<p>We offer a lot of good services for free here at JUMP news, so if you're a lawyer, would you reach out to us and help us review our contract pro bono? Contact us at </p>
+
+<p>Let us know who we sold our soul to.</p>
+
+<p>-Y.T.<br>
+GOSSIP GIRL XOXO</p>
+
+TEXT
+
+Article.find_or_create_by(title: "Request: Are we Breaching our Pantheon Contract?", byline: "Yours Truly, We the People", stub: "legal-request", blurb: "Are you a lawyer? Let us know if we're breaking the law!", content: content, published: true, fake_created_at: "Two days ago")
+
+content = <<-TEXT
+
+<p>WELCOME TO BIRDWATCH, the first chapter of our continuing series on Nexus City’s latest Mayoral Candidate, Colin Redwing!</p>
+
+<img src="/assets/falcon_running.png" alt="He's running" />
+
+<p>Many have called Colin Redwing the Joe Jackson of acting, and the Joe Jackson of music (though that's contested), and many in the CAPES BIZ call the Falcon the Joe Jackson of crimefighting, to which this publication agrees to call him a true Renaissance Man of being a Joe Jackson —</p>
+
+<p>But we’ll save the story of how his terrible parenting keeps pushing teens to self-destruction in a later article, when his lawyers have figured out our address.</p>
+
+<p>Today we’re here to tell you the story about how Yours Truly blew up the Falconwing and saved the world.</p>
+
+<p>You can’t make this shit up folks, and that’s why you come to JUMP News: Perceptive Minds Blown Dirt Cheap.</p>
+
+<p>So here’s the deal, we’ve been kinda coy about the fact that we saved the world, mostly because we think there’s a book deal in there somewhere and we’re not too proud for that. Also, because we protect our sources here. Also, because you’re getting a sly deal with these free stories anyhow, chump??</p>
+
+<p>But one or more members of Juveniles Unionizing for More Perception were involved in stopping the Wintry Midnight or the Snowpocalypse or whatever the news is calling it right now.</p>
+
+<p>Here’s a couple of facts for you in quick succession:</p>
+<ol>
+<li>When the need to save the world comes knocking, you often gotta move fast.</li>
+<li>Sometimes, the fastest way to get to the site of the potential apocalypse is stealing Colin Redwing’s sports car (KIDS: This is a crime! It’s called Joyriding! It’s as fun as it sounds!)</li>
+<li>Colin Redwing does not like his sports car being stolen.</li>
+</ol>
+
+<p>You can see where this is going.</p>
+
+<img src="/assets/joyriding.png" alt="But Officer, how could 'Joyriding' be bad?" />
+
+<p>Imagine you’re on your way to probable certain death (Y.T. note: Shut up, Ed.!) [Ed. You’re fired], driving a sick ass sports car at the apocalypse, when who should show up but a washed up actor in his wildly expensive jet? And imagine he announces himself by ripping off the hood of <s>your</s> his vehicle with his plane’s literal talons??</p>
+
+<p>Now I know you don’t come here for juicy gossip, so we’ll TL;DR and say the Falconwing had a weak spot (supervillains eat your heart out), and our buddy who trained with him knew it, so we shot this bad boy down, but that’s where it got MESSY for us.</p>
+
+<p>Turns out, the Falcon’s rep is earned: He had been studying us and had bird themed gadgets to fuck up everything that Yours Truly had going on. Y’all have speculated that some sort of foliage themed hero was involved in the world-saving. Well did you know that this hero was blasted by a bird-themed anti-fecund chemical spray right before they saved the world?</p>
+
+<p>Anyways, he fought us to a standstill, and say what you will about our avian-themed actor turned cape turned mayoral candidate, but not even the end of the world will stand between him and lecturing teens on their responsibilities, so make like the Falcon and BE PREPARED next time you question him on his tax plan!</p>
+
+<p>You know what we say here at JUMP news about the best way to end a lecture:</p>
+ 
+<img src="/assets/molotov.png" alt="Any time I had a problem and I threw a Molotov cocktail... Boom, right away I had a different problem."/>
+
+<p>So we snuck behind him and hit the self-destruct button on his downed jet. Here’s a tip for Falcon: If you’re gonna add a self-destruct button to your plane in the first act, it’s gonna be pressed in the third.</p>
+
+<img src="/assets/turkey.png" alt="Oh the Humanity" />
+
+The rest of what went down is still a secret to the non-initiated, but let it be known that the biggest threat to Yours Truly We The People and our team while trying to save the world was, for a while, our new Mayoral hopeful.
+
+More to come on the Falcon, but for now, just wanted to brag about blowing up his jet. 
+
+As always, remember our motto: WE NEED A GOOD LAWYER, FAST.
+
+-Y.T. Gossip Girl XOXO
+
+TEXT
+
+blurb = "<p>In the first part of our continuing series on washed up actor and Mayoral candidate Colin Redwing, we tell you the story of how we blew up the Falconwing.</p>"
+
+Article.find_or_create_by!(title: "Birdwatch Part 1: We Blew Up The Falconwing, LOL", byline: "Yours Truly, We the People", stub: "birdwatch-1", blurb: blurb, content: content, published: true, fake_created_at: "One Day Ago")
+
 
 content = <<-TEXT
 <p>We at JUMP news believe strongly in living well on a budget. See our past articles Eating Cheap in Nexus Slope, Which Hotel Pools are Easiest to Sneak Into, and of course, our interactive map on Where To Pee Good Without Paying.</p>
-<p>Here's a bargain for all you deal hunters: Free Superpowers.</p>
+<p>Here's a bargain for all you deal hunters: <b>Free Superpowers.</b></p>
 <p>Or at least, that's the headline we're going with: you gotta get clicks. But would you believe us if we told you you've already paid for your superpowers?</p>
-<p>SigTech takes billions of dollars in government contracts, and per exclusive information scooped here by Yours Truly We The People, is using that money and some clever biology to give some participants superpowers (Ed. Note - MOVE THIS TO TOP, YOU'RE BURYING THE LEDE UNDER YOUR BULLSHIT) [Y.T. Note - Oh, Ed. What would we do without you?]</p>
-<p>In fact, you or someone you know may have been saved by taxpayer-funded superpowers, with an anonymous superpowered source sharing directly with this publication that GREEN TIGER’S NEW PROTEGES WERE SUPAFIED IN SIGTECH LABS.</p>
+<p>SigTech takes billions of dollars in government contracts and tax breaks. What do they do with that? Per our anonymous superpowered source:</p> 
+<p><b>SigTech is using that money to give people superpowers!!!</b> (Ed. Note - MOVE THIS TO TOP, YOU'RE BURYING THE LEDE UNDER YOUR BULLSHIT) [Y.T. Note - Oh, Ed. What would we do without you?]</p>
+<img src="/assets/interocitor.png" alt="We have the technology!" />
+<p>In fact, you or someone you know may have been saved by taxpayer-funded superpowers, with an anonymous superpowered source sharing directly with this publication that <b>Green Tiger’s new proteges were all supafied in SigTech Labs!</b></p>
 <p>Don't believe us? See if they throw a lawsuit at us!</p>
 <p>Speaking of lawyers, we aren't lawyers but our understanding of the law is as follows:</p>
 <ol>
 <li>You pay the government</li>
 <li>The government pays SigTech</li>
-<li>Therefore you are SigTech’s boss!!!</li>
+<li>Therefore you are SigTech’s boss!</li>
 </ol>
 <p>That's right, since you're paying SigTech, you're entitled to a superpower. If you're the kind of perception-head we think you are, you'd JUMP to sign up today! </p>
 <p>Simply write to:</p>
@@ -47,4 +129,6 @@ blurb = <<-TEXT
 NEW SCOOP: SigTech is creating superheroes in a lab! Learn how Green Tiger's team was labgrown, and how you can demand your own!
 TEXT
 
-Article.find_or_create_by!(title: "Get Your Free Superpower Today!", byline: "Yours Truly, We the People", stub: "free-sigtech-superpowers", blurb: blurb, content: content, fake_created_at: "One Day Ago")
+Article.find_or_create_by!(title: "Get Your Free Superpower Today!", byline: "Yours Truly, We the People", stub: "free-sigtech-superpowers", blurb: blurb, content: content, published: true, fake_created_at: "One Day Ago")
+
+
