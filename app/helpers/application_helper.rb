2f220e3db472b780ba4240a965a6ff35430efa8b
Nicky <dovern42@gmail.com>
2025-12-12 16:27:24 -0500

    Fix email image URLs to use absolute paths

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,17 @@ module ApplicationHelper
     image_name = image_name.sub("/assets/", "")
     ActionController::Base.helpers.asset_path(image_name)
   end
+
+  def email_image_url(image_path)
+    # For emails: return full URL for external images, or asset URL for local assets
+    return image_path if image_path =~ /\A#{URI::DEFAULT_PARSER.make_regexp(['http', 'https'])}\z/
+    
+    # Handle /assets/ prefix
+    if image_path.start_with?("/assets/")
+      image_path = image_path.sub("/assets/", "")
+    end
+    
+    # Generate absolute URL with host
+    root_url.chomp('/') + asset_path(image_path)
+  end
 end
